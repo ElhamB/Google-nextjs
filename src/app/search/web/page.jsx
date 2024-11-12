1f236989fa447ebf29d30c4d90fe46353c203290
elham <bagheri_e@cyberno.ir>
2024-11-12 16:08:31 +0330

    feat;adds errors page

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -7,7 +7,6 @@ export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
   );
-  console.log('#res',response)
   if (!response.ok) return;
   const data = await response.json();
   const results = data.items;
